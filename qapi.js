8f69715f8612f6c7894ce669c7723cb691b903d9
Shams Arefin Khan <robeeen@gmail.com>
2020-10-18 15:53:52 +0600

    Some changes

---
+++ qapi.js
@@ -0,0 +1,37 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f8bb9a73d84d4001ec3aeb5",
+				"5f8bb9a73d84d4001ec3aeb7",
+				"5f8c0d3e3d84d4001ec3b0b2",
+				"5f8c0fa72e3a2a001faf7634"
+			]
+		},
+		"5f8bb9a73d84d4001ec3aeb5": {
+			"id": "5f8bb9a73d84d4001ec3aeb5",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f8bb9a73d84d4001ec3aeb7": {
+			"id": "5f8bb9a73d84d4001ec3aeb7",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5f8c0d3e3d84d4001ec3b0b2": {
+			"id": "5f8c0d3e3d84d4001ec3b0b2",
+			"pageUrl": "stage1",
+			"name": "stage1"
+		},
+		"5f8c0fa72e3a2a001faf7634": {
+			"id": "5f8c0fa72e3a2a001faf7634",
+			"pageUrl": "contact",
+			"name": "Contact"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
